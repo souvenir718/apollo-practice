5a7854d8a6423d3103b37b15d1ef80ae849405f1
souvenir718 <mongkey0@naver.com>
2021-01-21 16:32:00 +0900

    라우팅

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
+import { Route, Switch } from 'react-router';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import CreateLink from './CreateLink';
+import Header from './Header';
 import LinkList from './LinkList';
 
 function App() {
     // return <LinkList />;
-    return <CreateLink />;
+    return (
+        <div className="center w85">
+            <Header />
+            <div className="ph3 pv1 background-gray">
+                <Switch>
+                    <Route exact path="/" component={LinkList} />
+                    <Route exact path="/create" component={CreateLink} />
+                </Switch>
+            </div>
+        </div>
+    );
 }
 
 export default App;
